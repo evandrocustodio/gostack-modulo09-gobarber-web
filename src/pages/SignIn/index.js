cc345bc84552cde657000012104c4058f3d261c6
EVANDRO CUSTODIO GONCALVES <208396@E083554.int.cagece.com.br>
2019-10-12 16:42:03 -0300

    Atualizações

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
+import * as Yup from 'yup';
 
 export default function SignIn() {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Email Inválido')
+      .required('Email Obrigatório'),
+    password: Yup.string()
+      .min(6, 'No mínimo 6 caracteres')
+      .required('Password Obrigatório'),
+  });
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt='Logo' />
-      <form>
-        <input type='email' placeholder='Seu E-mail' />
-        <input type='password' placeholder='Sua senha secreta' />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name='email' type='email' placeholder='Seu E-mail aqui' />
+        <Input
+          name='password'
+          type='password'
+          placeholder='Sua senha secreta'
+        />
         <button type='submit'>Acessar</button>
         <Link to='/register'>Criar Conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
+import * as Yup from 'yup';
 
 export default function SignUp() {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Email Inválido')
+      .required('Email Obrigatório'),
+    password: Yup.string().required('Password Obrigatório'),
+  });
+  function handleSubmit(data) {
+    console.log(data);
+  }
   return (
     <>
       <img src={logo} alt='Logo' />
-      <form>
-        <input placeholder='Nome Completo' />
-        <input type='email' placeholder='Seu E-mail' />
-        <input type='password' placeholder='Sua senha secreta' />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name='name' placeholder='Nome Completo' />
+        <Input name='email' type='email' placeholder='Seu E-mail' />
+        <Input
+          name='password'
+          type='password'
+          placeholder='Sua senha secreta'
+        />
         <button type='submit'>Criar Conta</button>
         <Link to='/'>Já tenho conta</Link>
-      </form>
+      </Form>
     </>
   );
 }
